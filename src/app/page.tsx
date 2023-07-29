6e7c1f60b97a3ad6e75fc6829cdd23d3d6be2eab
MarLapuz <lapuzmar@gmail.com>
2023-07-30 02:04:21 +0800

    Adjusted the start illustration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,9 +44,9 @@ export default function Home() {
       </Container>
       <div
         className={classNames(
-          'mask-radial-faded pointer-events-none relative z-[-1] my-[-12.8rem] h-[60rem] overflow-hidden',
+          'mask-radial-faded pointer-events-none relative z-[-1] mx-auto my-[-12.8rem] h-[60rem] w-[min(1200px,100%)] overflow-hidden',
           '[--color:#7877C6] before:absolute before:inset-0 before:bg-radial-faded before:opacity-[0.4]',
-          'after:absolute after:-left-1/2 after:top-1/2 after:h-[142.8%] after:w-[200%] after:rounded-[50%] after:border-t after:border-[rgba(120,_119,_198,_0.4)] after:bg-background'
+          'after:absolute after:-left-1/2 after:top-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[50%] after:border-t after:border-[rgba(120,_119,_198,_0.4)] after:bg-background'
         )}
       >
         <StarsIllustration />
