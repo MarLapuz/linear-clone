ac1c1cfdd50c68f2d4b7b9b056ef06d5cfc5462d
MarLapuz <lapuzmar@gmail.com>
2023-08-02 17:30:20 +0800

    Added keyboard illustration and functionalities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,9 +25,7 @@ export default function Home() {
       >
         <StarsIllustration />
       </div>
-      <Container>
-        <UnlikeAnyTool />
-      </Container>
+      <UnlikeAnyTool />
     </>
   );
 }
